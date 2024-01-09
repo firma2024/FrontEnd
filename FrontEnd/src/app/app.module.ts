dad6b3f0a48849df5f6ef85638bde48f96d33ac9
Kevyn Prada <koprada@javeriana.edu.co>
2024-01-09 09:59:05 -0500

    created loginComponent

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NotFoundComponent } from './home/not_found/not-found/not-found.component';
+import { NotFoundComponent } from './home/not-found/not-found.component';
 import { HomeComponent } from './home/home/home.component';
+import { LoginComponent } from './modules/authentication/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
