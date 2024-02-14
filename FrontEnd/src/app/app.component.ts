69915de19d5a15b702d90d91c1c59f02bafa7fdb
Kevyn Prada <koprada@javeriana.edu.co>
2024-02-14 08:40:11 -0500

    special pages

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'FrontEnd';
+  title = 'FIRMA';
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Verificar si la ruta actual es "/login", "/register" o "/recover"
+        this.isSpecialPage();
+      }
+    });
+  }
+
+  isSpecialPage(): boolean {
+    const currentUrl = this.router.url;
+    return currentUrl === '/login' || currentUrl === '/register' || currentUrl === '/recover';
+  }
 }
