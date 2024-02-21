0a9e88dd769d3923cb3e328dd7dc21d9e25cb0b0
Kevyn Prada <koprada@javeriana.edu.co>
2024-02-20 20:47:48 -0500

    variables main

--- FrontEnd/src/app/modules/main-menu/main-menu.component.ts
+++ FrontEnd/src/app/modules/main-menu/main-menu.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.css'
 })
-export class MainMenuComponent {
+export class MainMenuComponent implements OnInit{
+  nombreEmpresa: string = 'Bran Holding Abogados & Asociados';
+  direccionEmpresa: string = '';
+  activeProcess: string = '';
+  lawyersRegister: string = '';
+  favorProcess: string = '';
+  againstProcess: string = '';
+
+  ngOnInit() {
+  }
 
 }
