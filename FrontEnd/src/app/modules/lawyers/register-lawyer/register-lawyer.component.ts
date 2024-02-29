3b91611a30d2b020ac5992cfab178feb7d1a4440
Juan Páez <jdpluc302@gmail.com>
2024-02-29 16:13:25 -0500

    create lawyer error

--- FrontEnd/src/app/modules/lawyers/register-lawyer/register-lawyer.component.ts
+++ FrontEnd/src/app/modules/lawyers/register-lawyer/register-lawyer.component.ts
@@ -2,21 +2,21 @@ import { Component } from '@angular/core';
 import { UserService } from '../../../services/user.service';
 import { TipoDocumento } from '../../../shared/model/doc.tipo';
 import { TipoAbogado } from '../../../shared/model/user/user.tipo';
-
+import { HttpErrorResponse } from '@angular/common/http';
+import { MensajeResponse } from '../../../shared/model/message';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-register-lawyer',
   templateUrl: './register-lawyer.component.html',
   styleUrl: './register-lawyer.component.css',
 })
 export class RegisterLawyerComponent {
-  
   nombreUsuario: string = '';
   nombreCompleto: string = '';
   correoElectronico: string = '';
   identificacion: string = '';
   telefono: string = '';
-  imageUrl:string | null = null;
-
+  imageUrl: string | null = null;
 
   selectedTypeDoc: TipoDocumento | null = null;
   selectedSpecialization: TipoAbogado | null = null;
@@ -62,24 +62,36 @@ export class RegisterLawyerComponent {
       }
     );
   }
+
   crearAbogado() {
-    this.userService.agregarAbogado(
-      this.nombreUsuario,
-      this.correoElectronico,
-      parseInt(this.telefono),
-      parseInt(this.identificacion),
-      this.nombreUsuario,
-      this.selectedTypeDoc!,
-      [this.selectedSpecialization!],
-      parseInt(localStorage.getItem('firmaId')!)
-    ).subscribe(
-      (response) => {
-        console.log('Abogado agregado correctamente:', response);
-      },
-      (error) => {
-        console.error('Error al agregar el abogado:', error);
-      }
-    );
+    this.userService
+      .validarAgregarAbogado(
+        this.nombreUsuario,
+        this.correoElectronico,
+        parseInt(this.telefono),
+        parseInt(this.identificacion),
+        this.nombreUsuario,
+        this.selectedTypeDoc!,
+        [this.selectedSpecialization!],
+        parseInt(localStorage.getItem('firmaId')!)
+      )
+      .subscribe(
+        (response: MensajeResponse) => {
+          console.log('Abogado agregado correctamente:', response);
+        },
+        (error: HttpErrorResponse) => {
+          if (error.status === 409) {
+            Swal.fire({
+              icon: 'error',
+              iconColor:'#AA2535',
+              title: 'Error',
+              confirmButtonColor: '#AA2535',
+              confirmButtonText: 'Okay',
+              text: 'El abogado no puede ser ingresado porque la identificación o el nombre de usuario ya existen.',
+            });
+          }
+        }
+      );
   }
 
   onSpecializationChange($event: any) {}
